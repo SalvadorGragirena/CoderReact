16f0bd68771a3846df393827132dbfb7558887a2
Salvadorgit <gragirenasalvador@gmail.com>
2023-07-18 20:02:53 -0400

    Entrega Final

--- src/components/Compra.jsx
+++ src/components/Compra.jsx
@@ -44,9 +44,9 @@ const Compra = () => {
         <h1 className="main-title">Contacto</h1>
         <form onSubmit={handleSubmit(enviar)}>
 
-            <input type="text" placeholder="Ingresá tu nombre" {...register("nombre")} />
-            <input type="email" placeholder="Ingresá tu e-mail" {...register("email")} />
-            <input type="phone" placeholder="Ingresá tu teléfono" {...register("telefono")} />
+            <input type="text" placeholder="INgrese su nombre" {...register("nombre")} />
+            <input type="email" placeholder="IIngrese su e-mail" {...register("email")} />
+            <input type="phone" placeholder="Ingrese su telfono" {...register("telefono")} />
 
             <button className="enviar" type="submit">Enviar</button>
 
